3a99697d96376541069a56af8cf24c34927f8f5f
Claudiu <shout@claudiu.mn>
2022-08-28 14:55:32 +0300

    Expose UITextView style properties

--- Sources/PagoUIKit/ImprovedTextView.swift
+++ Sources/PagoUIKit/ImprovedTextView.swift
@@ -11,12 +11,38 @@ public class ImprovedTextView : UIView {
     public var placeholder: String?
     
     public var validationErrorText: String? {
-        didSet {
-            updateValidation()
+        didSet { updateValidation() }
+    }
+    
+    public var borderColor: UIColor? {
+        set { textView.layer.borderColor = newValue?.cgColor }
+        get {
+            guard let cgColor = textView.layer.borderColor else { return nil }
+            return UIColor(cgColor: cgColor)
         }
     }
     
-    private let textView = UIKit.UITextView()
+    public var borderWidth: CGFloat {
+        set { textView.layer.borderWidth = newValue }
+        get { return textView.layer.borderWidth }
+    }
+    
+    public var cornerRadius: CGFloat {
+        set { textView.layer.cornerRadius = newValue }
+        get { return textView.layer.cornerRadius }
+    }
+    
+    public var mainFont: UIFont? {
+        set { textView.font = newValue }
+        get { return textView.font }
+    }
+    
+    public var mainTextColor: UIColor? {
+        set { textView.textColor = newValue }
+        get { return textView.textColor }
+    }
+    
+    private weak var textView: UITextView!
     
     override public init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,35 +55,38 @@ public class ImprovedTextView : UIView {
     }
     
     private func commonInit() {
+        let textView = UITextView()
+        
         textView.delegate = self
         
         textView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(textView)
         
-        let margin = 10.0
+        textView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        textView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        textView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        textView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         
-        textView.leadingAnchor.constraint(equalTo: leadingAnchor,
-                                          constant: margin).isActive = true
-        textView.trailingAnchor.constraint(equalTo: trailingAnchor,
-                                           constant: -margin).isActive = true
-        textView.topAnchor.constraint(equalTo: topAnchor,
-                                      constant: margin).isActive = true
-        textView.bottomAnchor.constraint(equalTo: bottomAnchor,
-                                         constant: -margin).isActive = true
+        textView.textContainerInset = UIEdgeInsets(top: 20,
+                                                   left: 20,
+                                                   bottom: 20,
+                                                   right: 20)
         
-        layer.cornerRadius = 10
-        layer.borderWidth = 2
+        self.textView = textView
         
+        mainFont = UIFont.systemFont(ofSize: 17)
+        mainTextColor = UIColor.darkText
+        cornerRadius = 10
+        borderWidth = 2
         updateValidation()
     }
     
     private func updateValidation() {
-        if let errorText = validationErrorText {
-            layer.borderColor = UIColor(rgb: 0xFF5151).cgColor
-            return
+        if validationErrorText != nil {
+            borderColor = UIColor(rgb: 0xFF5151)
+        } else {
+            borderColor = UIColor(rgb: 0xEFF2F7)
         }
-        
-        layer.borderColor = UIColor(rgb: 0xEFF2F7).cgColor
     }
 }
 
